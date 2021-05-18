c55e52e91e79c1659e5e8a4f1df5527fb1adb009
Nikki Ricks <nikki.ricks@envato.com>
2021-05-18 14:16:25 +1000

    Add more heroku secret keys

--- index.js
+++ index.js
@@ -40,17 +40,22 @@ const TOKEN_PATH = 'token.json';
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
+
+
+
 function authorize(credentials, callback) {
+  let GOOGLE_TOKEN = {"access_token": process.env.GOOGLE_ACCESS_TOKEN,"refresh_token": process.env.GOOGLE_REFRESH_TOKEN,"scope":"https://www.googleapis.com/auth/spreadsheets.readonly","token_type":"Bearer","expiry_date":1615265305502}
+  
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
+  // fs.readFile(TOKEN_PATH, (err, token) => {
+    // if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(GOOGLE_TOKEN);
     callback(oAuth2Client);
-  });
+  // });
 }
 
 /**
